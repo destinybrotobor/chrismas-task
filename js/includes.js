80bef7a6ee022aff53cfb4e1308cdcf444c64605
destinybrotobor <destinybrotos@programmerscity.com>
2026-01-27 16:02:33 +0100

    Create Other Sections of the Home Page

--- js/includes.js
+++ js/includes.js
@@ -25,3 +25,38 @@ fetch('components/service_area_section.html')
     .then(data => {
         document.getElementById('service_area_section').innerHTML = data;
     });
+
+// Include Mission & Vision Section
+fetch('components/mission_section.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('mission_vision_section').innerHTML = data;
+    });
+
+// Include Growth Section
+fetch('components/growth_section.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('growth_section').innerHTML = data;
+    })
+
+// Include How It Works Section
+fetch('components/howitworks.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('how_it_works_section').innerHTML = data;
+    })
+
+// Include Case Study Section
+fetch('components/case_study_section.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('case_study_section').innerHTML = data;
+    })
+
+// Include Business Strategy Section
+fetch('components/business_strategy.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('business_strategy_section').innerHTML = data;
+    });
